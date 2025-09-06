35912500edbdfdbe932bffa53c8a791a54bcfe5a
oizaeljr <izael.oliveirajr@sptech.school>
2025-09-06 16:36:57 -0300

    Telas adicionadas

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function abrirHome() {
+  window.location.href = "index.html"
+}
+
+function abrirSobreMim() {
+  window.location.href = "sobreMim.html"
+}
+
+function abrirFormacao() {
+  window.location.href = "formacao.html"
+}
+
+function abrirPortfolio() {
+  window.location.href = "portfolio.html"
+}
+
+function abrirContato() {
+  window.location.href = "contato.html"
+}
